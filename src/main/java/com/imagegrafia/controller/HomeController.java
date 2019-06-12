312c13266b2976728b5786cfd3238c9543ac40da
Ashish Yadhuvanshi <ashishkvs@gmail.com>
2019-06-12 11:50:06 +0530

    updated home controller

--- src/main/java/com/imagegrafia/controller/HomeController.java
+++ src/main/java/com/imagegrafia/controller/HomeController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/home")
+@RequestMapping("/index")
 public class HomeController {
 
 	@GetMapping
